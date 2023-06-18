9e73d8d7d18241902d7f464ac39bb2e9d8b79c12
Radolf1 <ryelebewere32@gmail.com>
2023-06-18 08:18:51 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (True)
+ */
+int main(void)
+{
+int fig;
+for (fig = '0'; fig <= '9'; fig++)
+{
+putchar(fig);
+if (fig != 9)
+{
+putchar(',');
+}
+}
+putchar('\n');
+return (0);
+}
