9476239b747f883a6df1eda68628176690554afe
Radolf1 <ryelebewere32@gmail.com>
2023-06-18 10:43:12 -0700

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,18 +6,18 @@
  * Return: Always 0 (True)
  */
 int main(void)
-{       
-        int first, second;
+{
+	int first, second;
 for (first = 0; first <= 98; first++)
 {
-for (second = first + 1; second <= 99; second++)
+for (second =first + 1; second <= 99; second++)
+{
 putchar((first / 10) + '0');
 putchar((first % 10) + '0');
 putchar(' ');
 putchar((second / 10) + '0');
 putchar((second % 10) + '0');
 if (first == 98 && second == 99)
-{
 continue;
 putchar(',');
 putchar(' ');
