1d0db552a39b232a37dbbca3fa6112a9c35aaf60
Radolf1 <ryelebewere32@gmail.com>
2023-06-18 10:29:25 -0700

    task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (True)
+ */
+int main(void)
+{       
+        int first, second;
+for (first = 0; first <= 98; first++)
+{
+for (second = first + 1; second <= 99; second++)
+putchar((first / 10) + '0');
+putchar((first % 10) + '0');
+putchar(' ');
+putchar((second / 10) + '0');
+putchar((second % 10) + '0');
+if (first == 98 && second == 99)
+{
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
