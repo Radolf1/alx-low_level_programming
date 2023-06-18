29bed20b0f784184a83ab77fdf10fb8ba23a870c
Radolf1 <ryelebewere32@gmail.com>
2023-06-18 07:46:23 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (True)
+ */
+int main(void)
+{
+	char reverse;
+for (reverse = 'z'; reverse <= 'a'; reverse++)
+putchar(reverse);
+putchar('\n');
+return (0);
+}
