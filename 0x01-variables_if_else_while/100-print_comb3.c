c266ac11d9863aa2466015cf24c7a8dff5f87872
Radolf1 <ryelebewere32@gmail.com>
2023-06-18 08:51:54 -0700

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (True)
+ */
+int main(void)
+{
+	int num1, num2;
+for (num1 = '0'; num1 < '9'; num1++)
+{
+for (num2 = num1 + 1; num2 <= '9'; num2++)
+{
+if (num2 != num1)
+putchar(num2);
+putchar(num1);
+if (num1 != '8' && num2 != '9')
+break;
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
