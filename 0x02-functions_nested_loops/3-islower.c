8b616ea647ca9a5d29e16987f305efef34160b82
Radolf1 <ryelebewere32@gmail.com>
2023-06-20 07:37:00 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower - check for lower characters
+ * return: 1 (true)
+ */
+int _islower(int c)
+{
+if (c > 96 && c < 123)
+{
+return (1);
+}
+return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int islower(int c);
+int _islower(int c);
 int alpha(int c);
 int sign(int n);
 int abs(int);
