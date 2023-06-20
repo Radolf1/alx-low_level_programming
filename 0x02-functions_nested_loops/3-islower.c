586812c6f9d21c718adb5d0f2622d050cdd80df4
Radolf1 <ryelebewere32@gmail.com>
2023-06-20 07:45:30 -0700

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * _islower - check for lower characters
- * c: will be used
- * return: 1 (true)
+ * @c: will be used
+ * return: 1 for c and 0 for any character
  */
 int _islower(int c)
 {
