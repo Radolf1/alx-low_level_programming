1a69d89f3b241ead6c3a30b73f22d5aa9c61c12f
Radolf1 <ryelebewere32@gmail.com>
2023-06-20 21:13:30 -0700

    task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Display first 52 fibonacci numbers
+ * REturn: succes
+ */
+int main(void)
+{
+	int  x =0;
+	long y = 1, z =2;
+while (x < 50)
+{
+if (i == 0)
+printf("%ld", y);
+else if (x == 1)
+printf(", %ld", z);
+else
+{
+z += y;
+y = z - y;
+printf(", %ld", z);
+}
+++x;
+}
+printf("\n");
+return (0);
+}
